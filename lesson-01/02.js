049d610db7b006c04722eff6280178dd28201c48
GurskiyMark <kotempu@gmail.com>
2025-07-31 18:53:54 +0300

    default

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,15 +1,16 @@
-let n = 3;
-let factorial = 1;
-    for(let i = 1; i <= n; i++){
-        factorial *= i;
-    }
-
-// function factorial(n) {
-//     let result = 1;           
-//     for (let i = 1; i <= n; i++) {  
-//         result *= i;          
+// let n = 3;
+// let factorial = 1;
+//     for(let i = 1; i <= n; i++){
+//         factorial *= i;
 //     }
-//     return result;            
-// }
 
-// console.log(factorial(3));   
+
+
+let number = 10;
+let factorial = 1;
+let counter = 1;
+    while (counter <= number) {
+        factorial = factorial * counter;  // factorial *= counter
+        counter ++;
+        
+    }
