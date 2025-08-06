97180df3f356c1ead55061ae8e0e77291302c085
GurskiyMark <kotempu@gmail.com>
2025-08-07 00:56:26 +0300

    function MAX number

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,32 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+// function findLargest(a,b,c) {
+//     let bigNumber = 0;
+//     if((a > b) && (a > c)) {
+//         bigNumber = a;
+//     } else if ((b > a) && (b > c)) {
+//         bigNumber = b;
+//     } else if ((c > a) && (c > b)) {
+//         bigNumber = c;
+//     }
+//     return bigNumber;
+// }
+
+// function findLargest(a, b, c) {
+//   return Math.max(a, b, c);
+// }
+
+
+function findLargest(a, b, c) {
+  let bigNumber = a;
+
+  if (b > bigNumber) {
+    bigNumber = b;
+  }
+
+  if (c > bigNumber) {
+    bigNumber = c;
+  }
+
+  return bigNumber;
+}
